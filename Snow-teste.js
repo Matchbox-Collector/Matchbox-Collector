fd0f32c290bae01c2b4df8e16b00e21199f85779
Matchbox-Collector <90335600+Matchbox-Collector@users.noreply.github.com>
2022-07-11 19:03:15 -0300

    Update Snow-teste.js

--- Snow-teste.js
+++ Snow-teste.js
@@ -16,7 +16,7 @@ function iecompattest()
   return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
 }
 
-var i, doc_width = 800, doc_height = 600;
+var i, doc_width = 800, doc_height = 300;
  
 if (ns6up)
 {
